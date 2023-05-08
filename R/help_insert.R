3306287663caf9de21f1c926c086c37b664582b8
zq2323 <782903342@qq.com>
2023-05-08 09:38:36 +0800

    use magrittr for R version < 4.1

--- R/help_insert.R
+++ R/help_insert.R
@@ -1,3 +1,5 @@
+library(magrittr)
+
 #' reformat the line of template by keywords
 #'
 #' @param template the template read from .txt
@@ -21,6 +23,7 @@ reformat_row <- function(template, row_item, add_item){
 #' @param .header_config a file path to import the template header
 #' @importFrom rstudioapi insertText getActiveDocumentContext setCursorPosition
 #' @importFrom purrr map iwalk
+#' @importFrom magrittr %>%
 #' @keywords internal
 help_insert_header <- function(.header_config = options()$gpphelper$headerTemplate){
     if(!is.null(.header_config)){
@@ -31,18 +34,18 @@ help_insert_header <- function(.header_config = options()$gpphelper$headerTempla
 
     #retrieve current package dependencies
     pkg <- renv::dependencies(path = rstudioapi::getActiveDocumentContext()$path)$Package
-    pkg_version <- purrr::map(pkg, function(x){ packageVersion(x) |> as.character())  |> purrr::flatten_chr()})
-    import_pkg <- paste0(pkg, " ", pkg_version)
-    template_header <- template_header |>
-        reformat_row("date", format(Sys.time(), "%Y-%m-%d")) |>
-        reformat_row("Software", R.version$version.string) |>
-        reformat_row("import", import_pkg)
-    template_header <- c(template_header, " ")
-    pos <- Map(c, 1:length(template_header), 1)
-    purrr::iwalk(pos, \(x, idx) rstudioapi::insertText(list(x), paste0(template_header[idx], "\n")))
-    rstudioapi::setCursorPosition(c(length(template_header), Inf))
+    pkg_version <- purrr::map(pkg, function(x){ packageVersion(x) %>% as.character()})  %>% purrr::flatten_chr()
+import_pkg <- paste0(pkg, " ", pkg_version)
+template_header <- template_header %>%
+    reformat_row("date", format(Sys.time(), "%Y-%m-%d")) %>%
+    reformat_row("Software", R.version$version.string) %>%
+    reformat_row("import", import_pkg)
+template_header <- c(template_header, " ")
+pos <- Map(c, 1:length(template_header), 1)
+purrr::iwalk(pos, function(x, idx) rstudioapi::insertText(list(x), paste0(template_header[idx], "\n")))
+rstudioapi::setCursorPosition(c(length(template_header), Inf))
 
-    invisible()
+invisible()
 }
 
 #' insert template comment before current line
@@ -50,6 +53,7 @@ help_insert_header <- function(.header_config = options()$gpphelper$headerTempla
 #' @param .comment_config a file path to import the template comment
 #' @importFrom rstudioapi insertText getActiveDocumentContext setCursorPosition
 #' @importFrom purrr iwalk map
+#' @importFrom magrittr %>%
 #' @keywords internal
 help_insert_comment <- function(.comment_config = options()$gpphelper$commentTemplate){
     if(!is.null(.comment_config)){
@@ -63,7 +67,7 @@ help_insert_comment <- function(.comment_config = options()$gpphelper$commentTem
     current_row <- getActiveDocumentContext()$selection[[1]]$range$start[1]
     pos <- Map(c, current_row:(current_row + length(template_comment) - 1), 1)
 
-    purrr::iwalk(pos, \(x, idx) rstudioapi::insertText(list(x), paste0(template_comment[idx], "\n")))
+    purrr::iwalk(pos, function(x, idx) rstudioapi::insertText(list(x), paste0(template_comment[idx], "\n")))
     rstudioapi::setCursorPosition(c(current_row + 1, Inf))
 
     invisible()
