a4deb2695a3b13714e4b81a32e74fc6d3c62cf20
zq2323 <782903342@qq.com>
2023-05-09 20:39:09 +0800

    Update help_insert.R

--- R/help_insert.R
+++ R/help_insert.R
@@ -8,6 +8,9 @@ library(magrittr)
 #' @keywords internal
 reformat_row <- function(template, row_item, add_item){
 
+    if(!any(grepl(row_item, template, ignore.case = TRUE))){
+        return(template)
+    }
     origin_index <- which(grepl(row_item, template, ignore.case = TRUE))
     origin <- template[origin_index]
     new_row <- paste0(c(origin, rep(paste0("#", strrep(" ", nchar(origin)-1)), length(add_item)-1)), " ", add_item)
@@ -25,27 +28,38 @@ reformat_row <- function(template, row_item, add_item){
 #' @importFrom purrr map iwalk
 #' @importFrom magrittr %>%
 #' @keywords internal
-help_insert_header <- function(.header_config = options()$gpphelper$headerTemplate){
-    if(!is.null(.header_config)){
-        template_header <- readLines(options()$gpphelper$headerTemplate)
-    }else{
-        template_header <- readLines(system.file('template/header.txt', package = "gpphelper"))
-    }
+help_insert_header <- function(opts = options()$gpphelper){
+
+    auth <- auth_init()
+    settings <- read_opts(opts = opts)
+    template_header <- settings %>%
+        dplyr::filter(author %in% auth) %>%
+        dplyr::select(header) %>%
+        dplyr::pull() %>%
+        purrr::flatten_chr() %>%
+        decodeChar()
+    author <- decodeChar(auth)
 
     #retrieve current package dependencies
     pkg <- renv::dependencies(path = rstudioapi::getActiveDocumentContext()$path)$Package
     pkg_version <- purrr::map(pkg, function(x){ packageVersion(x) %>% as.character()})  %>% purrr::flatten_chr()
-import_pkg <- paste0(pkg, " ", pkg_version)
-template_header <- template_header %>%
-    reformat_row("date", format(Sys.time(), "%Y-%m-%d")) %>%
-    reformat_row("Software", R.version$version.string) %>%
-    reformat_row("import", import_pkg)
-template_header <- c(template_header, " ")
-pos <- Map(c, 1:length(template_header), 1)
-purrr::iwalk(pos, function(x, idx) rstudioapi::insertText(list(x), paste0(template_header[idx], "\n")))
-rstudioapi::setCursorPosition(c(length(template_header), Inf))
+    import_pkg <- paste0(pkg, " ", pkg_version)
+    template_header <- template_header %>%
+        reformat_row("author", author) %>%
+        reformat_row("program name", fs::path_file(rstudioapi::getActiveDocumentContext()$path)) %>%
+        reformat_row("Compound/Study/Analysis", paste0(c(get0("W_ANALYSIS", envir = .GlobalEnv, ifnotfound = NULL),
+                                                       get0("W_COMPOUND", envir = .GlobalEnv, ifnotfound = NULL),
+                                                       get0("W_STUDY", envir = .GlobalEnv, ifnotfound = NULL)),
+                                                       collapse = "/")) %>%
+        reformat_row("# Date", format(Sys.time(), "%Y-%m-%d")) %>%
+        reformat_row("Software", R.version$version.string) %>%
+        reformat_row("import", import_pkg)
+    template_header <- c(template_header, " ")
+    pos <- Map(c, 1:length(template_header), 1)
+    purrr::iwalk(pos, function(x, idx) rstudioapi::insertText(list(x), paste0(template_header[idx], "\n")))
+    rstudioapi::setCursorPosition(c(length(template_header), Inf))
 
-invisible()
+    invisible()
 }
 
 #' insert template comment before current line
