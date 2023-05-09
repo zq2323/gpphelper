9596f57643fca2a8926848c0bbaf6682726d0786
zq2323 <782903342@qq.com>
2023-05-09 20:41:04 +0800

    Create encrypt.R

---
+++ R/encrypt.R
@@ -0,0 +1,20 @@
+# https://stackoverflow.com/questions/74424135/r-encoding-and-decoding-data
+asc <- function(x) { strtoi(charToRaw(x), 16L) }
+chr <- function(n) { rawToChar(as.raw(n)) }
+encrypt <- function(x, key, alphabet = ascii_chars) {
+    s <- strsplit(x, "")
+    sapply(s, \(y) {
+        i <- match(y, alphabet)
+        paste(key[i], collapse = "")
+    })
+}
+
+ascii_printable <- 32:126
+ascii_chars <- sapply(ascii_printable, chr)
+encryptKey <- readRDS(system.file('encryptKey.rds', package = "gpphelper"))
+encodeChar <- function(x){
+    encrypt(x, encryptKey, ascii_chars)
+}
+decodeChar <- function(e){
+    encrypt(e, ascii_chars, encryptKey)
+}
